d9e322ccb343679938047fe167f397057d11f6c4
polux0 <afidelo@protonmail.com>
2023-10-30 11:17:42 +0100

    fix: glow does not  happen more then once in 3 seconds, and it becomes removed after 0.5 second;

--- ux/economicMediaGlow.js
+++ ux/economicMediaGlow.js
@@ -1,56 +1,77 @@
-let hoverTimer; // This will store the timer
-const delayDuration = 1000; // Set delay to 1000ms (1s)
+let hoverTimer; // This will store the timer for delay
+let glowDurationTimer; // This will store the timer for glow duration
+let lastGlowTime = 0; // To store the last time the glow effect occurred
+const delayDuration = 3000; // Set delay to 3000ms (5s)
+
 function setEconomicMediaGlowListeners() {
     const containers = document.querySelectorAll('.available');
     const economicMediaButton = document.getElementById('bookOrbsButton');
 
-    if(!economicMediaButton.classList.contains('activeButton')){
+    if (!economicMediaButton.classList.contains('activeButton')) {
         containers.forEach(container => {
             container.addEventListener('mouseover', function(event) {
                 if (event.target.tagName === 'P') {
-                    // Clear any existing timer to reset the delay if another hover event is detected
+                    // Check if the last glow was more than 3 seconds ago
+                    const currentTime = new Date().getTime();
+                    if (currentTime - lastGlowTime < delayDuration) return;
+
+                    // Clear any existing timers
                     clearTimeout(hoverTimer);
-                    
-                    // Start a new timer
-                    hoverTimer = setTimeout(() => handleMouseOver(economicMediaButton), delayDuration);
+                    clearTimeout(glowDurationTimer);
+
+                    // Start a new timer for delay
+                    hoverTimer = setTimeout(() => {
+                        handleMouseOver(economicMediaButton);
+                        lastGlowTime = new Date().getTime(); // Update the last glow time
+
+                        // Start the glow duration timer
+                        glowDurationTimer = setTimeout(() => {
+                            handleMouseOut(economicMediaButton);
+                        }, 500); // 0.5s glow duration
+
+                    }, delayDuration);
                 }
             });
-      
+
             container.addEventListener('mouseout', function(event) {
                 if (event.target.tagName === 'P') {
-                    // Clear the timer to prevent hover effect if mouse is moved out before delay completes
+                    // Clear the timers
                     clearTimeout(hoverTimer);
+                    clearTimeout(glowDurationTimer);
                     handleMouseOut(economicMediaButton);
                 }
             });
         });
-    }  
-  }
-  
-  function handleMouseOver(element) {
+    }
+}
+
+function handleMouseOver(element) {
     if (!element.classList.contains('activeButton')) {
         element.style.background = 'white';
         element.style.boxShadow = '0 0 100px white';
-        element.style.transition = 'background 1.0s ease, box-shadow 1.0s ease';
-    }
-    else {
+        element.style.transition = 'background 0.5s ease, box-shadow 0.5s ease';
+    } else {
         console.log('element is already active', element);
     }
-  }
-  
-  function handleMouseOut(element) {
+}
+
+function handleMouseOut(element) {
+    // Clear the glow duration timer
+    clearTimeout(glowDurationTimer);
+
     if (!element.classList.contains('activeButton')) {
         element.style.background = '';
         element.style.boxShadow = '';
     }
-  }
-  function removeEconomicMediaGlowListeners() {
+}
+
+function removeEconomicMediaGlowListeners() {
     const containers = document.querySelectorAll('.available');
-  
+
     containers.forEach(container => {
         container.removeEventListener('mouseover', handleMouseOver);
         container.removeEventListener('mouseout', handleMouseOut);
     });
-  }
+}
 
-export {setEconomicMediaGlowListeners, removeEconomicMediaGlowListeners}
+export { setEconomicMediaGlowListeners, removeEconomicMediaGlowListeners }
